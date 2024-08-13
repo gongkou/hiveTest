849cd95c9984d185ed4afb575c31150591895536
zhangzy <815080696@qq.com>
2024-08-13 11:33:22 +0800

    Initial commit

---
+++ javatest/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * ${NAME}
+ *
+ * @author ${USER}
+ * @version 1.0
+ * @date ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("1");
+    }
+}
